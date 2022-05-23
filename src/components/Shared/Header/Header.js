422188ec7ebbbb5119d1589c64fc32a3fa80c7b3
habib-sc <habib97.cs@gmail.com>
2022-05-23 09:19:48 +0600

    showing login button if user logged in

--- src/components/Shared/Header/Header.js
+++ src/components/Shared/Header/Header.js
@@ -1,7 +1,17 @@
+import { signOut } from 'firebase/auth';
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link, NavLink } from 'react-router-dom';
+import auth from '../../../firebase.init';
 
 const Header = () => {
+
+    const [user, loading, error] = useAuthState(auth);
+
+    const logOut = () => {
+        signOut(auth);
+    };
+
     return (
         <div className='bg-base-100 fixed top-0 w-full z-40 shadow border border-b-gray-100'>
             <div class="container mx-auto px-4 navbar">
@@ -12,9 +22,16 @@ const Header = () => {
                     <ul class="menu menu-horizontal p-0">
                         <li className='ml-3'><NavLink to='/'>Home</NavLink></li>
                         <li className='ml-3'><NavLink to='/about'>About</NavLink></li>  
-                        <li className='ml-3'><NavLink to='/contact'>Contact</NavLink></li>  
-                        <li className='ml-3'><NavLink to='/login'>Login</NavLink></li>  
-                        <li className='ml-3'><NavLink to='/register'>Register</NavLink></li>  
+                        <li className='ml-3'><NavLink to='/contact'>Contact</NavLink></li>
+                        {user?
+                            <li className='ml-3'><button className='btn btn-outline'><NavLink to='/'>Logout</NavLink></button></li>
+                            :
+                            <>
+                                <li className='ml-3'><NavLink to='/login'>Login</NavLink></li>  
+                                <li className='ml-3'><NavLink to='/register'>Register</NavLink></li>
+                            </>
+                        }  
+                          
                     </ul>
                 </div>
             </div>
