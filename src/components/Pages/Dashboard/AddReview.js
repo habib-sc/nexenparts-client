086dd56942339b1c9cd71a5081588a84ea3ea166
habib-sc <habib97.cs@gmail.com>
2022-05-25 23:07:58 +0600

    add product feature added

--- src/components/Pages/Dashboard/AddReview.js
+++ src/components/Pages/Dashboard/AddReview.js
@@ -7,33 +7,35 @@ const AddReview = () => {
     const onSubmit = data => console.log(data);
 
     return (
-        <div className='border px-5 py-10 rounded-lg shadow-lg'>
-            <h2 className='text-2xl font-semibold font-secondary mb-5'>Add Review Here</h2>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                
-                <div className='grid grid-cols-1 max-w-[500px] gap-4'>
-                    <div className='grid gird-cols-1'>
-                        <label htmlFor="">Your Name</label>
-                        <input className='border border-gray-400 py-2 px-2 rounded-lg' type="text" {...register("reviewerName", {required: true})} />
-                    </div>
-                    <div className='grid gird-cols-1'>
-                        <label htmlFor="">Review Description</label>
-                        <textarea className='border border-gray-400 py-2 px-2 rounded-lg' type="text" {...register("description", {required: true})} />
-                    </div>
-                    <div className='grid gird-cols-1'>
-                        <label htmlFor="">Address (Optional)</label>
-                        <input className='border border-gray-400 py-2 px-2 rounded-lg' type="text" {...register("reviewerLocation")} />
-                    </div>
-                    <div className='grid gird-cols-1'>
-                        <label htmlFor="">Upload Image (Optional)</label>
-                        <input className='border border-gray-400 py-2 px-2 rounded-lg' type="file" {...register("img")} />
-                    </div>
+        <div className='border px-5 py-10 rounded-lg shadow-lg w-full'>
+            <h2 className='text-2xl text-center font-semibold font-secondary mb-5'>Add Review Here</h2>
+            <div className='w-full flex justify-center'>
+                <form onSubmit={handleSubmit(onSubmit)}>
                     
-                    <button className='btn btn-secondary mt-5 text-white w-full'>Submit</button>
+                    <div className='grid grid-cols-1 w-full lg:w-[600px] md:w-[500px] gap-4'>
+                        <div className='grid gird-cols-1'>
+                            <label htmlFor="">Your Name</label>
+                            <input className='border border-gray-400 py-2 px-2 rounded-lg' type="text" {...register("reviewerName", {required: true})} />
+                        </div>
+                        <div className='grid gird-cols-1'>
+                            <label htmlFor="">Review Description</label>
+                            <textarea className='border border-gray-400 py-2 px-2 rounded-lg' type="text" {...register("description", {required: true})} />
+                        </div>
+                        <div className='grid gird-cols-1'>
+                            <label htmlFor="">Address (Optional)</label>
+                            <input className='border border-gray-400 py-2 px-2 rounded-lg' type="text" {...register("reviewerLocation")} />
+                        </div>
+                        <div className='grid gird-cols-1'>
+                            <label htmlFor="">Upload Image (Optional)</label>
+                            <input className='border border-gray-400 py-2 px-2 rounded-lg' type="file" {...register("img")} />
+                        </div>
+                        
+                        <button className='btn btn-secondary mt-5 text-white w-full'>Submit</button>
 
-                </div>
+                    </div>
 
-            </form>
+                </form>
+            </div>
         </div>
     );
 };
--- src/components/Pages/Dashboard/Admin/AddProduct.js
+++ src/components/Pages/Dashboard/Admin/AddProduct.js
@@ -1,15 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
+import { toast } from 'react-toastify';
+import myAxios from '../../../../myAxios/myAxios';
+import Spinner from '../../../Shared/Spinner/Spinner';
 
 const AddProduct = () => {
-    const { register, handleSubmit, formState: { errors } } = useForm();
+    const { register, handleSubmit, formState: { errors }, reset } = useForm();
+    const [loading, setLoading] = useState(false);
 
-    const onSubmit = data => console.log(data);
+    // Handling adding Product 
+    const handleAddProduct = data => {
+        const image = data.img[0];
+        const formData = new FormData();
+        formData.append('image', image);
+
+        setLoading(true);
+
+        // Sending image to image bb server 
+        const url = `https://api.imgbb.com/1/upload?key=${process.env.REACT_APP_IMAGE_STORAGE_KEY}`;
+        fetch(url, {
+            method: 'POST',
+            body: formData
+        })
+        .then(res => res.json())
+        .then(result => {
+            if(result.success){
+                const img = result.data.url;
+                const productData = {
+                    name: data.itemName,
+                    price: data.price,
+                    quantity: data.quantity,
+                    minOrderQty: data.minOrderQty,
+                    description: data.description,
+                    img: img
+                };
+
+                // Send Product info to database 
+                ( async () => {
+                    const { data } = await myAxios.post('http://localhost:5000/add-product', productData);
+
+                    if(data.insertedId){
+                        toast.success('Product Added Successfully');
+                        reset();
+                        setLoading(false);
+                    }
+                    else{
+                        toast.error('Faild To Add Product');
+                        setLoading(false);
+                    }
+                })();
+                
+            }
+        });
+    };
+
+    if(loading) {
+        return <Spinner></Spinner>
+    }
+    
+
 
     return (
         <div className='border px-5 py-10 rounded-lg shadow-lg'>
             <h2 className='text-2xl font-semibold font-secondary mb-5'>Add Item Here</h2>
-            <form onSubmit={handleSubmit(onSubmit)}>
+            <form onSubmit={handleSubmit(handleAddProduct)}>
                 
                 <div className='grid grid-cols-1 lg:grid-cols-4 gap-4 items-end'>
                     <div className='grid gird-cols-1'>
@@ -18,15 +72,15 @@ const AddProduct = () => {
                     </div>
                     <div className='grid gird-cols-1'>
                         <label htmlFor="">Price</label>
-                        <input className='border border-gray-400 py-2 px-2 rounded-lg' type="text" {...register("price", {required: true})} />
+                        <input className='border border-gray-400 py-2 px-2 rounded-lg' type="number" {...register("price", {required: true})} />
                     </div>
                     <div className='grid gird-cols-1'>
                         <label htmlFor="">Item Quantity</label>
-                        <input className='border border-gray-400 py-2 px-2 rounded-lg' type="text" {...register("quantity", {required: true})} />
+                        <input className='border border-gray-400 py-2 px-2 rounded-lg' type="number" {...register("quantity", {required: true})} />
                     </div>
                     <div className='grid gird-cols-1'>
                         <label htmlFor="">Min Order Quantity</label>
-                        <input className='border border-gray-400 py-2 px-2 rounded-lg' type="text" {...register("minOrderQty", {required: true})} />
+                        <input className='border border-gray-400 py-2 px-2 rounded-lg' type="number" {...register("minOrderQty", {required: true})} />
                     </div>
                     <div className='grid gird-cols-1'>
                         <label htmlFor="">Upload Image</label>
