6bc42e3f126257703d66e1f0e7a30712a2f8cf98
habib-sc <habib97.cs@gmail.com>
2022-05-26 00:02:01 +0600

    reviw adding

--- src/components/Pages/Dashboard/AddReview.js
+++ src/components/Pages/Dashboard/AddReview.js
@@ -1,16 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
+import { toast } from 'react-toastify';
+import myAxios from '../../../myAxios/myAxios';
+import Spinner from '../../Shared/Spinner/Spinner';
 
 const AddReview = () => {
-    const { register, handleSubmit, formState: { errors } } = useForm();
+    const { register, handleSubmit, formState: { errors }, reset } = useForm();
+    const [loading, setLoading] = useState(false);
 
-    const onSubmit = data => console.log(data);
+    // Handling adding review 
+    const handleAddReview = data => {
+        const image = data.img[0];
+        const formData = new FormData();
+        formData.append('image', image);
+
+        setLoading(true);
+
+        let img;
+
+        if(image){
+            // Sending image to image bb server 
+            const url = `https://api.imgbb.com/1/upload?key=${process.env.REACT_APP_IMAGE_STORAGE_KEY}`;
+            fetch(url, {
+                method: 'POST',
+                body: formData
+            })
+            .then(res => res.json())
+            .then(result => {
+                if(result.success){
+                    img = result.data.url;
+                    
+                }
+            });
+        }
+
+        const reviewData = {
+            reviewerName: data.reviewerName,
+            description: data.description,
+            reviewerLocation: data.reviewerLocation,
+            img: img
+        };
+
+        // Send review info to database 
+        ( async () => {
+            const { data } = await myAxios.post('http://localhost:5000/add-review', reviewData);
+
+            if(data.insertedId){
+                toast.success('Review Added Successfully');
+                reset();
+                setLoading(false);
+            }
+            else{
+                toast.error('Failed To Add Review');
+                setLoading(false);
+            }
+        })();
+
+
+
+    };
+
+    if(loading) {
+        return <Spinner></Spinner>
+    }
 
     return (
         <div className='border px-5 py-10 rounded-lg shadow-lg w-full'>
             <h2 className='text-2xl text-center font-semibold font-secondary mb-5'>Add Review Here</h2>
             <div className='w-full flex justify-center'>
-                <form onSubmit={handleSubmit(onSubmit)}>
+                <form onSubmit={handleSubmit(handleAddReview)}>
                     
                     <div className='grid grid-cols-1 w-full lg:w-[600px] md:w-[500px] gap-4'>
                         <div className='grid gird-cols-1'>
