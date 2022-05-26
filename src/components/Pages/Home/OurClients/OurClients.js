e4d9c69b30155f8df111befc3eb07e987bec390f
habib-sc <habib97.cs@gmail.com>
2022-05-26 20:37:18 +0600

    changed color of client section

--- src/components/Pages/Home/OurClients/OurClients.js
+++ src/components/Pages/Home/OurClients/OurClients.js
@@ -2,32 +2,32 @@ import React from 'react';
 
 const OurClients = () => {
     return (
-        <div className='bg-gray-400 pb-2'>
+        <div className='bg-secondary pb-2'>
             <div className='container px-4 mx-auto my-20'>
-                <h1 class="text-center text-3xl font-bold mb-10 uppercase pt-5">Our Clients</h1>
-                    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
-                            <div className='border lg:px-20'>
+                <h1 class="text-center text-3xl font-bold text-white mb-10 uppercase pt-5">Our Clients</h1>
+                    <div class="grid grid-cols-2 lg:grid-cols-4 gap-8">
+                            <div className='lg:px-20'>
                                 <img className='w-full' src="https://i.ibb.co/Bwg82SL/client-img1-hov.png" alt="" />
                             </div>
-                            <div className='border lg:px-20'>
+                            <div className='lg:px-20'>
                                 <img className='w-full' src="https://i.ibb.co/SrHRNnP/client-img2-hov.png" alt="" />
                             </div>
-                            <div className='border lg:px-20'>
+                            <div className='lg:px-20'>
                                 <img className='w-full' src="https://i.ibb.co/L0L4FDt/client-img5-hov.png" alt="" />
                             </div>
-                            <div className='border lg:px-20'>
+                            <div className='lg:px-20'>
                                 <img className='w-full' src="https://i.ibb.co/GJWxWvF/client-img6-hov.png" alt="" />
                             </div>
-                            <div className='border lg:px-20'>
+                            <div className='lg:px-20'>
                                 <img className='w-full' src="https://i.ibb.co/b7PKNFL/client-img7-hov.png" alt="" />
                             </div>
-                            <div className='border lg:px-20'>
+                            <div className='lg:px-20'>
                                 <img className='w-full' src="https://i.ibb.co/HzzmfWm/client-img8-hov.png" alt="" />
                             </div>
-                            <div className='border lg:px-20'>
+                            <div className='lg:px-20'>
                                 <img className='w-full' src="https://i.ibb.co/Qcmf6YV/client-img12-hov.png" alt="" />
                             </div>
-                            <div className='border lg:px-20'>
+                            <div className='lg:px-20'>
                                 <img className='w-full' src="https://i.ibb.co/nzZD6dq/client-img14-hov.png" alt="" />
                             </div>
                          
